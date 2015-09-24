c82180c748e126a62a309e54ddb8ca820c48490c
Serhii Veretilo <serhii.veretilo@w3.co>
2015-09-24 19:40:10 +0300

    view

--- backend/controllers/FilmController.php
+++ backend/controllers/FilmController.php
@@ -17,4 +17,25 @@ class FilmController extends ActiveController
             ],
         ]);
     }
+    
+    public function actions()
+    {
+        return array_merge(parent::actions(), [
+            'index' => [
+                'class' => 'backend\controllers\actions\IndexAction',
+                'modelClass' => $this->modelClass,
+                //'sort' => ['post_date' => SORT_DESC],
+                //'params' => array('entityName' => 'Post'),
+            ],
+            'view' => [
+                'class' => 'backend\controllers\actions\ViewAction',
+                'modelClass' => $this->modelClass,
+            ],
+//            'delete' => [
+//                'class' => 'backend\controllers\actions\DeleteAction',
+//                'modelClass' => $this->modelClass,
+//            ],
+        ]);
+
+    }
 }
